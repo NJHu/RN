106bd5d0d5da5515c64b27f46dab263b86f26f32
胡鹏 <64hp@163.com>
2019-11-13 21:44:09 +0800

    初始化组件测试

---
+++ ComponentTest/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default class app extends Component {
+
+  render(){
+    return (
+        <View>
+          <Text>123</Text>
+        </View>
+    )
+  }
+}
