836b79d986b7e218bb43f508b5371cadd387c467
胡鹏 <64hp@163.com>
2019-11-13 22:30:09 +0800

    Update App.js

--- ComponentTest/App.js
+++ ComponentTest/App.js
@@ -8,17 +8,38 @@
 
 import React, {Component} from 'react';
 import {
-  View,
-  Text,
+    View,
+    Text,
+    StyleSheet,
+    Alert,
+    TextInput,
 } from 'react-native';
 
 export default class app extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            titleText: "Bird's Nest",
+            bodyText: 'This \nis \nnot\n really\n a\n bird\n ne\nst.',
+            value: '',
+        };
+        this._onPressTitle = this.onPressTitle.bind(this);
+    }
 
-  render(){
-    return (
-        <View>
-          <Text>123</Text>
-        </View>
-    )
-  }
+    render() {
+        return (
+            <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 100 }}
+                onChangeText={this._onPressTitle}
+                value={this.state.value}
+            />
+        )
+    }
+
+    onPressTitle(text) {
+        // Alert.alert(text);
+        this.setState({
+            value: text,
+        });
+    }
 }
